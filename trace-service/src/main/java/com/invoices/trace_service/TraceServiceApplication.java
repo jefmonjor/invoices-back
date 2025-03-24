f861b217d71178657b9a15af405ef85f9f4596bb
jefferson <jefmonjor@gmail.com>
2025-03-24 21:42:03 +0100

    feat:  Primer commit
     invoices-config archivos .yml de cada servicio.

---
+++ trace-service/src/main/java/com/invoices/trace_service/TraceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.invoices.trace_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraceServiceApplication.class, args);
+	}
+
+}
