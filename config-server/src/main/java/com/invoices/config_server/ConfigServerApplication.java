cb7b1ffc62cfc20cc5f90ef87d1a23c935b02e86
jefferson <jefmonjor@gmail.com>
2025-03-25 20:10:35 +0100

    fix: proyecto y config servicios

--- config-server/src/main/java/com/invoices/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/invoices/config_server/ConfigServerApplication.java
@@ -2,12 +2,12 @@ package com.invoices.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
-
 }
